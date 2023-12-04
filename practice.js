df3f0a0ca69eda4baf0bf60013877b8b52cf8626
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-12-05 02:22:15 +0300

    Refresher on Promises

---
+++ practice.js
@@ -0,0 +1,50 @@
+const users = [
+  {
+    name: "Brenda",
+    age: 31,
+    email: "brendafrenjo@gmail.com",
+  },
+  {
+    name: "Susan",
+    age: 25,
+    email: "susanlimo@gmail.com",
+  },
+  {
+    name: "Roman",
+    age: 27,
+    email: "romanlimbe@gmail.com",
+  },
+];
+
+function getUsers() {
+  setTimeout(() => {
+    output = "";
+    users.forEach((user, index) => {
+      output += `<li key=${index}>${user.name} (${user.email}) is ${user.age} years of age.</li>`;
+    });
+    let body = document.querySelector("#main");
+    body.innerHTML = output;
+  }, 1000);
+}
+
+function createUser(user) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      users.push(user);
+
+      let error = false;
+
+      if (!error) {
+        resolve();
+      } else {
+        reject("Error: Something went wrong!");
+      }
+    });
+  }, 2000);
+}
+
+createUser({
+  name: "Peter",
+  age: 22,
+  email: "peterkamakis@gmail.com",
+}).then(getUsers);
